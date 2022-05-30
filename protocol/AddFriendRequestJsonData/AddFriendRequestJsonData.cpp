511811b2d43560d80ea7e8b08d75e0576876c618
qin11152 <105280761@qq.com>
2022-05-30 21:22:03 +0800

    增加好友请求协议文件

---
+++ protocol/AddFriendRequestJsonData/AddFriendRequestJsonData.cpp
@@ -0,0 +1,46 @@
+#include "AddFriendRequestJsonData.h"
+
+AddFriendRequestJsonData::AddFriendRequestJsonData(const std::string& message/*=""*/)
+{
+    parse(message);
+}
+
+void AddFriendRequestJsonData::parse(const std::string& message/*=""*/)
+{
+    if (message.empty())
+    {
+        return;
+    }
+    ptree m_ptree;
+    std::stringstream sstream(message);
+    read_json(sstream, m_ptree);
+    m_strMyId = m_ptree.get<std::string>("MyId");
+    m_strFriendId = m_ptree.get<std::string>("FriendId");
+    m_strVerifyMsg = m_ptree.get<bool>("VerifyMsg");
+    return;
+}
+
+std::string AddFriendRequestJsonData::generateJson()
+{
+    StringBuffer strbuf;
+    Writer<rapidjson::StringBuffer> writer(strbuf);
+
+    writer.StartObject();
+
+    writer.Key("type");
+    writer.Int(static_cast<int>(m_strType));
+
+    writer.Key("MyId");
+    writer.String(m_strMyId.c_str());
+
+    writer.Key("FriendId");
+    writer.String(m_strFriendId.c_str());
+
+    writer.Key("VerifyMsg");
+    writer.String(m_strVerifyMsg.c_str());
+
+    writer.EndObject();
+
+    return strbuf.GetString();
+}
+
