e20454f5da74fba9ebb68e403556b234193739e1
qin11152 <105280761@qq.com>
2022-07-18 22:08:06 +0800

    [ADD] 增加日志模块

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "ui/LogInWidget/LogInWidget.h"
-#include "ui/ChatWidget/ChatWidget.h"
-#include "module/PublicFunction/PublicFunction.h"
+#include  "module/Log/Log.h"
 #include <QtWidgets/QApplication>
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    Logcxx::get_mutable_instance().Init("./log4cxx.properties");
+    _LOG(Logcxx::INFO, "test");
     LogInWidget w;
     w.show();
     /*ChatWidget* w = new ChatWidget("1");
