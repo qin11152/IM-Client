0f26ad152b0b3d8b116a91d37a977361e76cbbc1
qin11152 <105280761@qq.com>
2022-10-08 21:26:47 +0800

    [ADD] 增加了一个线程池，用于处理耗时任务

---
+++ module/ThreadPool/ThreadPool.cpp
@@ -0,0 +1,66 @@
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(int number)
+    :m_iThreadNumber(number)
+{
+}
+
+void ThreadPool::startPool()
+{
+    m_bRunning = true;
+    for (int i = 0; i < m_iThreadNumber; ++i)
+    {
+        std::thread t(&ThreadPool::doTask, this);
+        m_vecThread.push_back(move(t));
+    }
+}
+
+void ThreadPool::stopPool()
+{
+    //运行状态置为false
+    m_bRunning = false;
+    //逻辑和析构函数一样
+    m_dequeNotEmptyCV.notify_all();
+    for (auto& item : m_vecThread)
+    {
+        //回收线程资源
+        if (item.joinable())
+        {
+            item.join();
+        }
+    }
+}
+
+void ThreadPool::doTask()
+{
+    while (1)
+    {
+        //要获取的任务
+        ThreadTask task;
+        {
+            std::unique_lock<std::mutex> lck(m_mutex);
+            //正在运行状态且deque大小为空
+            /*if (m_bIsRunning && 0 == m_deqTask.size())
+            {
+                printf("thread id:%d,", GetCurrentThreadId());
+                printf("wiat state\n");
+                //没有任务的时候就等待
+                m_conDequeNotEmpty.wait(lck);
+            }*/
+            //等待任务队列不空或者状态为退出
+            m_dequeNotEmptyCV.wait(lck, [this]() {return !m_bRunning || !m_dequeTask.empty(); });
+            //不在运行状态且任务队列空了那就退出
+            if (!m_bRunning && m_dequeTask.empty())
+            {
+                return;
+            }
+            //有任务就把顶端的拿出来
+            task = move(m_dequeTask.front());
+            m_dequeTask.pop_front();
+        }
+        //出了上边的作用域,lck就自动解锁了
+        //执行任务
+        task();
+    }
+}
+
---
+++ module/ThreadPool/ThreadPool.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <functional>
+#include <future>
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <thread>
+#include "boost/noncopyable.hpp"
+
+using ThreadTask = std::function<void()>;
+
+class ThreadPool :public boost::noncopyable
+{
+public:
+    ThreadPool(int number);
+    ~ThreadPool();
+
+    void startPool();
+    void stopPool();
+    void doTask();
+
+    template<typename F,typename ...Args>
+    auto submit(F&& f,Args ...args)
+    {
+        std::function<decltype(f(args...))()> func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+        auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+        ThreadTask task = [task_ptr]() {(*task_ptr)(); };
+        m_dequeTask.push_back(task);
+    }
+
+private:
+
+    std::deque<ThreadTask> m_dequeTask;         //存放任务的队列
+    std::condition_variable m_dequeNotEmptyCV;  //deque非空通知
+    std::vector<std::thread> m_vecThread;   //线程池中的线程保存
+    std::mutex m_mutex;             //锁
+    int m_iThreadNumber{ 0 };       //线程池中子线程数量
+    bool m_bRunning{ false };       //是否在运行
+
+};
+
+
