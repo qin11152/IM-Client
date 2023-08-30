8520f795fbea289d6f4a72796c236f332b545796
qin11152 <1052080761@qq.com>
2023-08-30 18:35:40 +0800

    [MOD] 因为找到mingw版本的logcxx库，增加宏定义区分

--- module/Log/Log.cpp
+++ module/Log/Log.cpp
@@ -1,11 +1,70 @@
 ﻿#include "Log.h"
 #include <cstdarg>
 #include <boost/format.hpp>
+#include <string>
+
+#if defined(__MINGW32__) || defined(__MINGW64__)
+class Logcxx::Imp
+{
+public:
+	Imp()
+	{
+
+	}
+	~Imp()
+	{
+
+	}
+
+	void Init(const char* prop = "log4cxx.properties")
+	{
+		return;
+	}
+	void Print(Logcxx::Level level, std::string& buff)
+	{
+		return;
+	}
+};
+
+Logcxx::Logcxx(void)
+{
+	_pImp = new Imp();
+}
+
+
+Logcxx::~Logcxx(void)
+{
+	if (_pImp)
+	{
+		delete _pImp;
+	}
+}
+
+void Logcxx::Init(const char* prop)
+{
+	_pImp->Init(prop);
+}
+
+void Logcxx::Print(const char* file, long line, const char* funtion, Level level, const char* format, ...)
+{
+#ifdef _DEBUG
+	return;
+#endif
+
+	char buff[4096];
+	va_list ap;
+	va_start(ap, format);
+	vsnprintf(buff, 4096, format, ap);
+	va_end(ap);
+
+	std::string strlog = (boost::format(" %s %s %s : %s") % file % line % funtion % buff).str();
+
+	_pImp->Print(level, strlog);
+}
+#else
 #include <log4cxx/logger.h>  
 #include <log4cxx/basicconfigurator.h>  
 #include <log4cxx/propertyconfigurator.h>  
-#include <string>
-
 class Logcxx::Imp
 {
 public:
@@ -90,4 +149,5 @@ void Logcxx::Print(const char* file, long line, const char* funtion, Level level
 
 	_pImp->Print(level, strlog);
 }
+#endif
 
