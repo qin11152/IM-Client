b029d93b06d6e63dcfe4a76a2767a27f1a7ef754
qin11152 <60863859+qin11152@users.noreply.github.com>
2023-10-17 21:54:16 +0800

    [MOD] 修改发起群聊的协议

--- protocol/StartGroupChatJsonData/StartGroupChatJsonData.cpp
+++ protocol/StartGroupChatJsonData/StartGroupChatJsonData.cpp
@@ -2,66 +2,6 @@
 
 namespace protocol
 {
-	StartGroupChatInnerData::StartGroupChatInnerData(const std::string& message)
-	{
-		parse(message);
-	}
-
-	StartGroupChatInnerData::~StartGroupChatInnerData()
-	{
-	}
-
-	void StartGroupChatInnerData::parse(const std::string& message)
-	{
-		if (message.empty())
-		{
-			return;
-		}
-		ptree m_ptree;
-		std::stringstream sstream(message);
-		read_json(sstream, m_ptree);
-		m_strGroupName = m_ptree.get<std::string>("GroupName");
-		m_strGuid = "";
-		m_strStarterId = m_ptree.get<std::string>("StartId");
-		//从json数组中提取gruopchatid
-		for (auto& item : m_ptree.get_child("GroupChatId"))
-		{
-			m_vecGroupChat.push_back(item.second.get_value<std::string>());
-		}
-	}
-
-	std::string StartGroupChatInnerData::generateJson()
-	{
-		//rapisjson生成json
-		StringBuffer strbuf;
-		Writer<rapidjson::StringBuffer> writer(strbuf);
-
-		writer.StartObject();
-
-		writer.Key("type");
-		writer.Int(static_cast<int>(m_strType));
-
-		writer.Key("StarterId");
-		writer.String(m_strStarterId.c_str());
-
-		writer.Key("Guid");
-		writer.String(m_strGuid.c_str());
-
-		writer.Key("GroupName");
-		writer.String(m_strGroupName.c_str());
-
-		writer.Key("GroupChatId");
-		writer.StartArray();
-		for (auto& item : m_vecGroupChat)
-		{
-			writer.String(item.c_str());
-		}
-		writer.EndArray();
-
-		writer.EndObject();
-
-		return strbuf.GetString();
-	}
 
 	StartGroupJsonData::StartGroupJsonData(const std::string& message)
 	{
@@ -81,9 +21,14 @@ namespace protocol
 		ptree m_ptree;
 		std::stringstream sstream(message);
 		read_json(sstream, m_ptree);
-		m_iImageLenth = m_ptree.get<int>("ImageLength");
-		m_strImageSuffix = m_ptree.get<std::string>("ImageSuffix");
-		m_stInnerData.parse(m_ptree.get<std::string>("InnerData"));
+		m_strGroupName = m_ptree.get<int>("GroupName");
+		m_strImagePathInServer = m_ptree.get<std::string>("ImagePath");
+		m_strStarterId = m_ptree.get<std::string>("StartId");
+		//获取json对象中id数组
+		for (auto& item : m_ptree.get_child("GroupChatId"))
+		{
+			m_vecGroupChat.push_back(item.second.get_value<std::string>());
+		}
 	}
 
 	std::string StartGroupJsonData::generateJson()
@@ -96,14 +41,22 @@ namespace protocol
 		writer.Key("type");
 		writer.Int(static_cast<int>(m_strType));
 
-		writer.Key("ImageLength");
-		writer.Int(m_iImageLenth);
+		writer.Key("GroupName");
+		writer.String(m_strGroupName.c_str());
 
-		writer.Key("ImageSuffix");
-		writer.String(m_strImageSuffix.c_str());
+		writer.Key("ImagePath");
+		writer.String(m_strImagePathInServer.c_str());
 
-		writer.Key("InnerData");
-		writer.String(m_stInnerData.generateJson().c_str());
+		writer.Key("StartId");
+		writer.String(m_strStarterId.c_str());
+
+		writer.Key("GroupChatId");
+		writer.StartArray();
+		for (auto& item : m_vecGroupChat)
+		{
+		writer.String(item.c_str());
+		}
+		writer.EndArray();
 
 		writer.EndObject();
 
--- protocol/StartGroupChatJsonData/StartGroupChatJsonData.h
+++ protocol/StartGroupChatJsonData/StartGroupChatJsonData.h
@@ -1,34 +1,21 @@
 ﻿#pragma once
 
-#include "protocol/ImageJsonData/ImageJsonDataBase.h"
+#include "../JsonBaseData.h"
 
 namespace protocol
 {
-	class StartGroupChatInnerData : public ImageInnerJsonData
+	class StartGroupJsonData : public JsonBaseData
 	{
 	public:
-		StartGroupChatInnerData(const std::string& message = "");
-		~StartGroupChatInnerData()override;
+		StartGroupJsonData(const std::string& message = "");
+		~StartGroupJsonData();
 		void parse(const std::string& message)override;
 		std::string generateJson()override;
 
 		std::string m_strStarterId{ "" };
-		std::string m_strGuid{ "" };
 		std::string m_strGroupName{ "" };
+		std::string m_strImagePathInServer{ "" };
 		std::vector<std::string> m_vecGroupChat;
-
 		MessageType m_strType{ MessageType::StartGroupChat };
-
-	};
-
-	class StartGroupJsonData : public ImageJsonBaseData
-	{
-	public:
-		StartGroupJsonData(const std::string& message = "");
-		~StartGroupJsonData()override;
-		void parse(const std::string& message)override;
-		std::string generateJson()override;
-
-		StartGroupChatInnerData m_stInnerData;   //内部可包含一层数据，可为空
 	};
 }
